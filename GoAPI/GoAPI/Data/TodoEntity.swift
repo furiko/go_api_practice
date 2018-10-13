e8906778ea971e01e5bbfbb6f1f52dda1016fe9b
futa <>
2018-10-13 23:41:56 +0900

    Swift作成開始

---
+++ GoAPI/GoAPI/Data/TodoEntity.swift
@@ -0,0 +1,15 @@
+//
+//  TodoEntity.swift
+//  GoAPI
+//
+//  Created by futa on 2018/10/13.
+//  Copyright © 2018年 furiko. All rights reserved.
+//
+
+import Foundation
+
+struct Todo {
+    var Id: Int
+    var Name: String
+    
+}
---
+++ GoAPI/GoAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoAPI
+//
+//  Created by futa on 2018/10/13.
+//  Copyright © 2018年 furiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
